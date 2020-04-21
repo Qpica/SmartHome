b5959f354b12810ef5975714a5b2570036afc0a0
CSKenez <cs.k.kenez@gmail.com>
2020-04-21 17:24:02 +0200

    asdf

---
+++ smartHome/src/java/Room.java
@@ -0,0 +1,13 @@
+class Room {
+	
+	final int targetTemp;
+	int currentTemp;
+	String name;
+	
+	Room(int targetTemp, int currentTemp, String name){
+		this.targetTemp = targetTemp;
+		this.currentTemp = currentTemp;
+		this.name = name;
+	}
+	
+}
